e0f8c6c603dce66847669245b17b209fac20869b
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-06-14 23:46:04 +0300

    refactor

---
+++ lib/login_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            bottom: height / double.infinity,
+            height: height,
+            child: Image.asset('images/12.jpg'),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: TextStyle(fontSize: 30, color: Colors.white),
+                    fillColor: Colors.white,
+                    hoverColor: Colors.white),s
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
