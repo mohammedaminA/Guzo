8479a13129f668553a273550bf982820db3dabc3
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-06-15 17:31:17 +0300

    login screen

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -10,22 +10,54 @@ class LoginScreen extends StatelessWidget {
           constraints:
               BoxConstraints(maxHeight: size.height, maxWidth: size.width),
           decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.blueAccent, Colors.lightBlueAccent, Colors.blue],
-            ),
+            gradient: LinearGradient(colors: [
+              Colors.blueAccent,
+              Colors.lightBlueAccent,
+              Colors.blue
+            ], begin: Alignment.topLeft, end: Alignment.centerRight),
           ),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Expanded(
                 flex: 2,
-                child: Column(
-                  children: [
-                    Text('Login'),
-                  ],
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 36.0, horizontal: 24.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Login',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 60,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      Text(
+                        "Addis Ababa's top ride hailing app",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w100),
+                      )
+                    ],
+                  ),
                 ),
               ),
               Expanded(
                 flex: 5,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40),
+                    ),
+                  ),
+                ),
               )
             ],
           ),
