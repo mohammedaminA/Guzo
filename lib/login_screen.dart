02aaad40436e92ebe9f62cfa1be9b96d686fd5fa
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-06-15 17:53:51 +0300

    login screen

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -51,11 +51,33 @@ class LoginScreen extends StatelessWidget {
               Expanded(
                 flex: 5,
                 child: Container(
+                  width: size.width,
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(40),
                       topRight: Radius.circular(40),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Email Address',
+                            labelStyle: TextStyle(
+                                fontSize: 20,
+                                fontFamily: 'Roboto',
+                                color: Colors.black),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                                borderSide: BorderSide.none),
+                          ),
+                        )
+                      ],
                     ),
                   ),
                 ),
