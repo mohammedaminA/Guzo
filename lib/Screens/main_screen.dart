125883bb6ae1d176f758e0ffb1d8425ec84083cd
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-06-19 23:55:28 +0300

    google maps

--- lib/Screens/main_screen.dart
+++ lib/Screens/main_screen.dart
@@ -1,9 +1,26 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MainScreen extends StatelessWidget {
-  final user = FirebaseAuth.instance.currentUser;
+class MainScreen extends StatefulWidget {
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  Completer<GoogleMapController> _controllerMap = Completer();
+
+  GoogleMapController newMapController;
+
+  User user = FirebaseAuth.instance.currentUser;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +30,19 @@ class MainScreen extends StatelessWidget {
                 backgroundImage: NetworkImage(user.photoURL),
               ),
               onPressed: () {})),
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            myLocationButtonEnabled: true,
+            initialCameraPosition: MainScreen._kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controllerMap.complete(controller);
+              newMapController = controller;
+            },
+          )
+        ],
+      ),
     );
   }
 }
