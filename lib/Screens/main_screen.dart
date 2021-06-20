96a49736d1089ccd5d504346b0152c980ec38280
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-06-20 14:49:34 +0300

    refactor

--- lib/Screens/main_screen.dart
+++ lib/Screens/main_screen.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:guzo/widgets/divider.dart';
 
 class MainScreen extends StatefulWidget {
   static final CameraPosition _kGooglePlex = CameraPosition(
@@ -61,17 +62,118 @@ class _MainScreenState extends State<MainScreen> {
                   ),
                 ],
               ),
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.search,
-                    color: Colors.lightBlueAccent,
-                  ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text('Search Drop Off Location')
-                ],
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 24.0, vertical: 18.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 6.0,
+                    ),
+                    Text(
+                      'Hi there, ${user.displayName}',
+                      style: TextStyle(fontSize: 12.0),
+                    ),
+                    Text(
+                      'Where to?',
+                      style: TextStyle(fontSize: 12.0),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black54,
+                            blurRadius: 6.0,
+                            spreadRadius: 0.5,
+                            offset: Offset(0.7, 0.7),
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.search,
+                            color: Colors.lightBlueAccent,
+                          ),
+                          SizedBox(
+                            width: 10.0,
+                          ),
+                          Text('Search Drop Off Location')
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 24.0,
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.home,
+                          color: Colors.grey,
+                        ),
+                        SizedBox(
+                          width: 12.0,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Add home'),
+                            SizedBox(
+                              height: 4.0,
+                            ),
+                            Text(
+                              'Your home address',
+                              style: TextStyle(
+                                color: Colors.black54,
+                                fontSize: 12.0,
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    DividerWidget(),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.work,
+                          color: Colors.grey,
+                        ),
+                        SizedBox(
+                          width: 12.0,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Add work'),
+                            SizedBox(
+                              height: 4.0,
+                            ),
+                            Text(
+                              'Your work address',
+                              style: TextStyle(
+                                color: Colors.black54,
+                                fontSize: 12.0,
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),
