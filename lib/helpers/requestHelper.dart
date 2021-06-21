824d2d2b3c57b113b1ed8c45bf6e6a7b6289e34e
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-06-21 13:24:58 +0300

    requestHelpers

---
+++ lib/helpers/requestHelper.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class RequestHelper {
+  static Future<dynamic> getRequest(String url) async {
+    http.Response response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      String jsonData = response.body;
+      var decodedData = jsonDecode(jsonData);
+      return decodedData;
+    } else {
+      return "Something has gone worong!";
+    }
+  }
+}
