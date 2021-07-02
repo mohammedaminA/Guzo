73a29194e95ebe933e0430c5a29a150c90b62cbe
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-07-02 11:50:16 +0300

    prediction

--- lib/Screens/search_screen.dart
+++ lib/Screens/search_screen.dart
@@ -186,7 +186,7 @@ class _SearchScreenState extends State<SearchScreen> {
           print('Place Predictions:::: ');
           print(placesList);
           setState(() {
-            placePredictionsList = placesList;sasada
+            placePredictionsList = placesList;sasad
           });
         }
       }
